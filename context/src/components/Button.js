bc542517a1f141ad959f2fe00e6751c79e516ffd
snavdeepsingh <snavdeepsingh@gmail.com>
2020-05-05 00:31:43 -0500

    Added Context and created context store to manage state without using Redux.

---
+++ context/src/components/Button.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+/* How to extract value from context via 'this.context' */
+
+/* 
+class Button extends React.Component {
+    static contextType = LanguageContext;
+
+    render(){
+        console.log(this.context)
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
+        return (
+            <button className="ui button primary">{text}</button>
+        )
+    }
+}
+*/
+
+/* How to extract value from context via 'consumer' */
+
+class Button extends React.Component {
+    renderSubmit(language){
+        return language === 'english' ? 'Submit' : 'Voorleggen';
+    }
+
+    renderButton(color){
+        return (
+            <button className={`ui button ${color}`}>
+                    <LanguageContext.Consumer >
+                        {({language}) => this.renderSubmit(language) }
+                    </LanguageContext.Consumer>
+                </button>
+        )
+    }
+
+    render(){
+        return (
+            <ColorContext.Consumer >
+                {(color) => 
+                    this.renderButton(color)
+                }
+            </ColorContext.Consumer>
+        )
+    }
+}
+
+export default Button;
